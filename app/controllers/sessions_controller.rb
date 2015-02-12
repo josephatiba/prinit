edbc9002bdc08f5a7935a097b0d6628956089af5
Joey Atiba <joey.atiba@gmail.com>
2015-02-11 20:43:28 -0800

    renames some of the routes and the sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,4 @@
-# class Devise::SessionsController < DeviseController
+class Devise::SessionsController < DeviseController
 #   prepend_before_filter :require_no_authentication, only: [ :new, :create ]
 #   prepend_before_filter :allow_params_authentication!, only: :create
 #   prepend_before_filter :verify_signed_out_user, only: :destroy
@@ -78,4 +78,4 @@
 #       format.any(*navigational_formats) { redirect_to after_sign_out_path_for(resource_name) }
 #     end
 #   end
-# end
+end
