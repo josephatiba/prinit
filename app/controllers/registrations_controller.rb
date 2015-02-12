4734427013525a1070d0d1cf8c9b2061d6ccf1c2
Joey Atiba <joey.atiba@gmail.com>
2015-02-11 17:41:46 -0800

    moves the user registrations and omniauthcallbacks controllers to the controller directory. also adds current user email to the header when a user is signed in.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
-# before_filter :configure_sign_up_params, only: [:create]
-# before_filter :configure_account_update_params, only: [:update]
-
- def update_resource(resource, params)
+  def update_resource(resource, params)
     if resource.encrypted_password.blank? # || params[:password].blank?
-      resource.email = params[:email] if params[:email]
+      @resource.email = params[:email] if params[:email]
       if !params[:password].blank? && params[:password] == params[:password_confirmation]
         logger.info "Updating password"
         resource.password = params[:password]
@@ -17,5 +14,4 @@ class RegistrationsController < Devise::RegistrationsController
       resource.update_with_password(params)
     end
   end
-  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users, class_name: 'FormUser', :controllers => { omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations' }
 
   get '/home' => 'home#index'
