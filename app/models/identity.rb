1c8897011465a107fbdee142e4bfb915edef2893
Joey Atiba <joey.atiba@gmail.com>
2015-02-11 21:20:45 -0800

    removes incorrect user calls from identity.rb

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -27,16 +27,16 @@ class Identity
   def self.find_for_oauth(auth)
     identity = create(uid: auth.uid, provider: auth.provider) if identity.nil?
     identity = find_by(provider: auth.provider, uid: auth.uid)
-    user.identity.accesstoken = auth.credentials.token
-    user.identity.refreshtoken = auth.credentials.refresh_token
-    user.uidentity.name = auth.info.name
-    user.identity.email = auth.info.email
-    user.identity.nickname = auth.info.nickname
-    user.identity.image = auth.info.image
-    user.identity.phone = auth.info.phone
-    user.identity.urls = (auth.info.urls || "").to_json
-    user.identity.save
-    user.identity
+    identity.accesstoken = auth.credentials.token
+    identity.refreshtoken = auth.credentials.refresh_token
+    identity.name = auth.info.name
+    identity.email = auth.info.email
+    identity.nickname = auth.info.nickname
+    identity.image = auth.info.image
+    identity.phone = auth.info.phone
+    identity.urls = (auth.info.urls || "").to_json
+    identity.save
+    identity
   end
 
 
