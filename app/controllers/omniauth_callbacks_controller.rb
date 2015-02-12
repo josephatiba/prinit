60e34fc1f6783f345b0b8d08011fa041957665ed
Joey Atiba <joey.atiba@gmail.com>
2015-02-11 20:30:55 -0800

    undoes changes that made with the experimental code and adds auth path nicknames

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -11,21 +11,21 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     generic_callback( 'twitter' )
   end
 
-  def generic_callback( provider )
+  def generic_callback(provider)
     @identity = Identity.find_for_oauth env["omniauth.auth"]
 
     @user = @identity.user || current_user
     if @user.nil?
       @user = User.create( email: @identity.email || "" )
-      @identity.update_attribute( :user_id, @user.id )
+      @identity.update_attribute(:user_id, @user.id)
     end
 
     if @user.email.blank? && @identity.email
-      @user.update_attribute( :email, @identity.email)
+      @user.update_attribute(:email, @identity.email)
     end
 
     if @user.persisted?
-      @identity.update_attribute( :user_id, @user.id )
+      @identity.update_attribute(:user_id, @user.id)
       # This is because we've created the user manually, and Device expects a
       # FormUser class (with the validations)
       @user = FormUser.find @user.id
